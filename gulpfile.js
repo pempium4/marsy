6fa7e2bbdd442ce48a2e6c66bed8647b9dc7d2e8
Pempium4 <front.shipper@gmail.com>
2020-10-31 13:37:10 +0200

    Nav & raw header

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const gulpConcat = require('gulp-concat');
+const gulpCleanCSS = require('gulp-clean-css');
+const gulpUglify = require('gulp-uglify');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const gulpSass = require('gulp-sass');
+const gulpSourceMaps = require('gulp-sourcemaps');
+
+const gulpRename = require('gulp-rename');
+
+
+gulp.task('del', () => {
+    return del(['Build/*']);
+});
+gulp.task('styles', () =>{
+    return gulp.src('./SCSS/**/*.scss')
+        .pipe(gulpSourceMaps.init())
+        .pipe(gulpSass().on('Error', gulpSass.logError))
+        .pipe(gulpConcat('style.css'))
+        .pipe(gulpCleanCSS())
+        .pipe(gulpRename({
+            suffix: '.min'
+        }))
+        .pipe(gulpSourceMaps.write('./'))
+        .pipe(gulp.dest('./Build/'))
+        .pipe(browserSync.stream());
+});
+gulp.task('scripts', () => {
+    return gulp.src('./JS/**/*.js')
+        .pipe(gulpConcat('main.js'))
+        .pipe(gulpUglify({
+            toplevel: true
+        }))
+        .pipe(gulpRename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./Build'))
+        .pipe(browserSync.stream());
+});
+gulp.task('watch', () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./SCSS/**/*.scss', gulp.series('styles'));
+    gulp.watch('./JS/**/*.js', gulp.series('scripts'));
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts'), 'watch'));
